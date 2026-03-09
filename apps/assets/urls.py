9b5019c923e5cc356976a009476217a83596f85a
Elliot Price <elliot_price@hotmail.com>
2026-03-09 15:49:42 +0000

    Added all asset views, created pages for each, create, update and delete functions all work. Added a message to prints when a delete asset call is made to an asset that is currently assigned to a user

--- apps/assets/urls.py
+++ apps/assets/urls.py
@@ -5,4 +5,8 @@ app_name = "assets"
 
 urlpatterns = [
     path("", views.AssetListView.as_view(), name="asset_list"),
+    path("<int:pk>/", views.AssetDetailView.as_view(), name="asset_detail"),
+    path("create/", views.AssetCreateView.as_view(), name="asset_create"),
+    path("<int:pk>/update/", views.AssetUpdateView.as_view(), name="asset_update"),
+    path("<int:pk>/delete/", views.AssetDeleteView.as_view(), name="asset_delete"),
 ]
--- apps/assets/views.py
+++ apps/assets/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.db.models import ProtectedError
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from apps.assets.models import Asset
 
 
@@ -9,3 +12,33 @@ class AssetListView(ListView):
     template_name = "assets/asset_list.html"
     context_object_name = "assets"
     paginate_by = 10
+
+class AssetDetailView(DetailView):
+    model = Asset
+    template_name = "assets/asset_detail.html"
+    context_object_name = "asset"
+
+class AssetCreateView(CreateView):
+    model = Asset
+    template_name = "assets/asset_create.html"
+    fields = "__all__"
+    success_url = reverse_lazy("assets:asset_list")
+
+class AssetUpdateView(UpdateView):
+    model = Asset
+    template_name = "assets/asset_update.html"
+    context_object_name = "asset"
+    fields = "__all__"
+    success_url = reverse_lazy("assets:asset_list")
+
+class AssetDeleteView(DeleteView):
+    model = Asset
+    template_name = "assets/asset_delete.html"
+    success_url = reverse_lazy("assets:asset_list")
+
+    def form_valid(self, form):
+        try:
+            return super().form_valid(form)
+        except ProtectedError:
+            messages.error(self.request, "You cannot delete this asset, as it is currently assigned to a user")
+            return redirect("assets:asset_detail", pk=self.object.pk)

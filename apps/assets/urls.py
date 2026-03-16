625920170ba2eb64683254ecf9dcfe4e3cc87b75
Elliot Price <elliot_price@hotmail.com>
2026-03-16 16:21:41 +0000

    Hooked up asset_assignment views - ready for setting up styling

--- apps/assets/urls.py
+++ apps/assets/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path("create/", views.AssetCreateView.as_view(), name="asset_create"),
     path("<int:pk>/update/", views.AssetUpdateView.as_view(), name="asset_update"),
     path("<int:pk>/delete/", views.AssetDeleteView.as_view(), name="asset_delete"),
+    path("assignments/", views.AssetAssignmentListView.as_view(), name="asset_assignment_list"),
+    path("assignments/create/", views.AssetAssignmentCreateView.as_view(), name="asset_assignment_create"),
+    path("assignments/<int:pk>/", views.AssetAssignmentDetailView.as_view(), name="asset_assignment_detail"),
+    path("assignments/<int:pk>/update/", views.AssetAssignmentUpdateView.as_view(), name="asset_assignment_update"),
+    path("assignments/<int:pk>/delete/", views.AssetAssignmentDeleteView.as_view(), name="asset_assignment_delete"),
 ]
--- apps/assets/views.py
+++ apps/assets/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from apps.assets.models import Asset
+from apps.assets.models import Asset, AssetAssignment
 
 # Create your views here.
 class AssetListView(LoginRequiredMixin, ListView):
@@ -42,6 +42,39 @@ class AssetDeleteView(LoginRequiredMixin, UserPassesTestMixin , DeleteView):
         except ProtectedError:
             messages.error(self.request, "You cannot delete this asset, as it is currently assigned to a user")
             return redirect("assets:asset_detail", pk=self.object.pk)
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+class AssetAssignmentListView(LoginRequiredMixin, ListView):
+    model = AssetAssignment
+    template_name = "assets/asset_assignment_list.html"
+    context_object_name = "asset_assignments"
+    paginate_by = 10
+
+class AssetAssignmentDetailView(LoginRequiredMixin, DetailView):
+    model = AssetAssignment
+    template_name = "assets/asset_assignment_detail.html"
+    context_object_name = "asset_assignment"
+
+class AssetAssignmentCreateView(LoginRequiredMixin, CreateView):
+    model = AssetAssignment
+    template_name = "assets/asset_assignment_create.html"
+    fields = ["user", "asset", "date_given"]
+    success_url = reverse_lazy("assets:asset_assignment_list")
+
+class AssetAssignmentUpdateView(LoginRequiredMixin, UpdateView):
+    model = AssetAssignment
+    template_name = "assets/asset_assignment_update.html"
+    context_object_name = "asset_assignment"
+    fields = ["user", "asset", "date_given"]
+    success_url = reverse_lazy("assets:asset_assignment_list")
+
+class AssetAssignmentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = AssetAssignment
+    template_name = "assets/asset_assignment_delete.html"
+    success_url = reverse_lazy("assets:asset_assignment_list")
+    context_object_name = "asset_assignment"
 
     def test_func(self):
         return self.request.user.is_staff
